8638eebe896c74727f630e382603917b06b61a46
ArquimedesFermin <arquimedesfermin17@gmail.com>
2020-06-08 19:18:34 -0400

    .

--- RTM.FormXamarin/RTM.FormXamarin/Views/Empleados/ModificarEmpleados.xaml.cs
+++ RTM.FormXamarin/RTM.FormXamarin/Views/Empleados/ModificarEmpleados.xaml.cs
@@ -10,6 +10,7 @@ using RTM.FormXamarin.Models;
 using RTM.FormXamarin.Models.Usuarios;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XF.Material.Forms.UI.Dialogs;
 
 namespace RTM.FormXamarin.Views.Empleados
 {
@@ -20,6 +21,165 @@ namespace RTM.FormXamarin.Views.Empleados
         {
             InitializeComponent();
             mostrarInformacionEmpleado(id);
+
+            EventModificar.Clicked += EventModificar_Clicked;      
+        }
+
+        private async void EventModificar_Clicked(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.AppSettings["ipServer"];
+
+            try
+            {
+                var id = IdEmpleado.Text;
+                var nombreV = Nombre.Text;
+                var apellidoV = Apellido.Text;
+                var sexoV = Sexo.SelectedIndex;
+                var direccionV = Direccion.Text;
+                var telefonoV = telefono.Text;
+                var cedulaV = cedula.Text;
+                var edadV = Edad.Text;
+                var fnV = FN.Date;
+                var rolV = Roles.SelectedIndex;
+                var passwordV = Pass.Text;
+                var userNameV = nUsuario.Text;
+                var emailV = Email.Text;
+                var check = Estado.IsSelected;
+
+
+
+                if (string.IsNullOrEmpty(nombreV))
+                {
+                    await DisplayAlert("Validacion", "Ingrese el nombre de Usuario", "Aceptar");
+                    Nombre.Focus();
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(apellidoV))
+                {
+                    await DisplayAlert("Validacion", "Ingrese el Apellido", "Aceptar");
+                    Apellido.Focus();
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(direccionV))
+                {
+                    await DisplayAlert("Validacion", "Ingrese la Dirección", "Aceptar");
+                    Direccion.Focus();
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(telefonoV))
+                {
+                    await DisplayAlert("Validacion", "Ingrese el telefono", "Aceptar");
+                    telefono.Focus();
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(cedulaV))
+                {
+                    await DisplayAlert("Validacion", "Ingrese la cedula", "Aceptar");
+                    cedula.Focus();
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(edadV))
+                {
+                    await DisplayAlert("Validacion", "Ingrese la Edad", "Aceptar");
+                    Edad.Focus();
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(fnV.ToString()))
+                {
+                    await DisplayAlert("Validacion", "Ingrese la fecha de nacimiento", "Aceptar");
+                    FN.Focus();
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(passwordV))
+                {
+                    await DisplayAlert("Validacion", "Ingrese el password", "Aceptar");
+                    Pass.Focus();
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(userNameV))
+                {
+                    await DisplayAlert("Validacion", "Ingrese el Nombre de usuario", "Aceptar");
+                    nUsuario.Focus();
+                    return;
+                }
+                if (string.IsNullOrEmpty(emailV))
+                {
+                    await DisplayAlert("Validacion", "Ingrese el Email", "Aceptar");
+                    Email.Focus();
+                    return;
+                }
+
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(connectionString);
+
+                var empleados = new Emple()
+                {
+                    EmpleadoId =int.Parse(id),
+                    Nombres = nombreV,
+                    Apellidos = apellidoV,
+                    Sexo = (Sexo.SelectedIndex == 0) ? false : true,
+                    Direccion = direccionV,
+                    Telefono = telefonoV,
+                    UserName = userNameV,
+                    Fecha_Nacimiento = fnV,
+                    Email = emailV,
+                    RolID = rolV,
+                    PasswordHash = passwordV,
+                    Cedula = cedulaV,
+                    LockoutEnabled = check
+
+                };
+
+                var json = JsonConvert.SerializeObject(empleados);
+                StringContent stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var request = await client.PostAsync("/api/Empleados/modificar", stringContent);
+
+                if (request.IsSuccessStatusCode)
+                {
+
+                    var responseJson = await request.Content.ReadAsStringAsync();
+                    var respuesta = JsonConvert.DeserializeObject<Request>(responseJson);
+
+                    if (respuesta.status)
+                    {
+                        await MaterialDialog.Instance.AlertAsync(message: "Usuario se modifico correctamente",
+                                   title: "Registro",
+                                   acknowledgementText: "Aceptar");
+                    }
+                    else
+                    {
+                        await MaterialDialog.Instance.AlertAsync(message: "Usuario no pudo modificarse correctamente",
+                                  title: "Registro",
+                                  acknowledgementText: "Aceptar");
+
+                    }
+
+                }
+                else
+                {
+                    await MaterialDialog.Instance.AlertAsync(message: "Error",
+                                    title: "Error",
+                                    acknowledgementText: "Aceptar");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await MaterialDialog.Instance.AlertAsync(message: ex.Message,
+                                    title: ex.Message,
+                                    acknowledgementText: "Aceptar");
+            }
+
+
         }
 
         private void mostrarInformacionEmpleado(int id)
@@ -30,7 +190,7 @@ namespace RTM.FormXamarin.Views.Empleados
             HttpClient client = new HttpClient();
 
             client.BaseAddress = new Uri(connectionString);
-            var request = client.GetAsync($"/api/Empleados/listaPorId/{id}").Result;
+            var request = client.GetAsync($"/api/Empleados/listaPorIdEmpleados/{id}").Result;
 
             if (request.IsSuccessStatusCode)
             {
@@ -56,9 +216,13 @@ namespace RTM.FormXamarin.Views.Empleados
                     Pass.Text = listaView.PasswordHash;
                     Roles.SelectedIndex = listaView.RolID;
                     Email.Text = listaView.Email;
+                    Estado.IsSelected = listaView.LockoutEnabled;
                 }
 
             }
         }
+
+
+
     }
 }
