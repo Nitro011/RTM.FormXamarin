ae956f1b78beebdcf7735fc1a663a57234392927
ANDRES EUGENIO GUERRERO CORDERO <andresgc1997@outlook.com>
2020-08-08 03:28:20 -0400

    VistasV20

---
+++ RTM.FormXamarin/RTM.FormXamarin/Models/Estilos/EstilosListView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RTM.FormXamarin.Models.Estilos
+{
+    public class EstilosListView
+    {
+        public int EstiloID { get; set; }
+        public int Estilo_No { get; set; }
+        public string Descripcion { get; set; }
+        public decimal CostoSTD { get; set; }
+        public decimal Ganancia { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string PattenNo { get; set; }
+        public string Last { get; set; }
+        public string Comentarios { get; set; }
+        public string Marcas { get; set; }
+        public string Modelos { get; set; }
+        public string TiposEstilos { get; set; }
+        public string Categorias { get; set; }
+        public string Materiales { get; set; }
+        public string PesosEstilos { get; set; }
+        public string Colores { get; set; }
+        public string Division { get; set; }
+        public string UnidadesMedidas { get; set; }
+        public string Estados { get; set; }
+    }
+}
--- RTM.FormXamarin/RTM.FormXamarin/Views/Estilos/GestionarEstilos.xaml.cs
+++ RTM.FormXamarin/RTM.FormXamarin/Views/Estilos/GestionarEstilos.xaml.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using PCLAppConfig;
+using RTM.FormXamarin.Models.Estilos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using RTM.FormXamarin.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XF.Material.Forms.UI.Dialogs;
 
 namespace RTM.FormXamarin.Views.Estilos
 {
@@ -15,12 +20,47 @@ namespace RTM.FormXamarin.Views.Estilos
         public GestionarEstilos()
         {
             InitializeComponent();
+            ListaEstilos();
             agregarNuevoEstilo.Clicked += AgregarNuevoEstilo_Clicked;
         }
 
         private async void AgregarNuevoEstilo_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Estilos.RegistrarEstilos());
+        }
+
+        private async void ListaEstilos()
+        {
+            string connectionString = ConfigurationManager.AppSettings["ipServer"];
+
+
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri(connectionString);
+            var request = client.GetAsync("/api/EstilosNuevos/EstilosList").Result;
+
+            if (request.IsSuccessStatusCode)
+            {
+                var responseJson = request.Content.ReadAsStringAsync().Result;
+                var response = JsonConvert.DeserializeObject<Request>(responseJson);
+
+                if (response.status)
+                {
+
+                    var listaView = JsonConvert.DeserializeObject<List<EstilosListView>>(response.data.ToString());
+
+                    listaEstilos.ItemsSource = listaView;
+
+
+                }
+                else
+                {
+                    await MaterialDialog.Instance.AlertAsync(message: "Error",
+                                   title: "Error",
+                                   acknowledgementText: "Aceptar");
+                }
+
+            }
         }
     }
 }
